b6ec9752683db7774230e6dc9b1e2f209aaf4a98
Soucouyant <duranr@Durans-MacBook-Air.local>
2022-03-31 12:51:59 -0400

    Non-working keydown event

--- Part1/entrypoint.py
+++ Part1/entrypoint.py
@@ -1,12 +1,36 @@
-import pygame 
+#Single Line Comments are so lame
+#Just have multi line
+#lame language
+#Required Comments ^
 
+import pygame 
+import random
+
+# Initialize
 pygame.init()
 screen = pygame.display.set_mode((400, 300))
+
+#Do things
+
+isBlue = True
+
+if isBlue: color = (0, 128, 255)
+else: color = (255, 100, 0)
+
+# draw
+pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 60))
+
+# Event Handler
 done = False
 
 while not done: 
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            isBlue = not isBlue
         if event.type == pygame.QUIT:
             done = True
-
-            pygame.display.flip()
+    
+    # Update Screen
+    pygame.display.flip()
+    
+    
