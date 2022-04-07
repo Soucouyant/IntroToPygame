c35cde583861dd63d3db9e006d266d3793d3dccc
Soucouyant <duranr@protonmail.com>
2022-04-07 11:26:50 -0400

    added font

---
+++ Part5/p5.py
@@ -0,0 +1,26 @@
+# Duran Ramlall
+# Part 5 of NerdParadise PyGame: Getting Started walkthrough
+# Thursday April 7th
+# TEJ4M1 P2 Gr 12
+
+# Import Statements
+import pygame 
+
+# Initialize
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+
+# Declarations
+clock = pygame.time.Clock()
+done = False
+
+while not done:
+    # Event Handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    # Update Screen
+    pygame.display.flip()
+    clock.tick(60)
+    
